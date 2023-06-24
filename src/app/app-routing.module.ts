c32b655ea12b4664002ac6805de0b461f36ef76b
Eito Takase <eito01178@gmail.com>
2023-06-24 17:15:58 +0900

    ng generate でコンポーネントとかモジュール生成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./routed/index/index.module').then((m) => m.IndexModule),
+  },
+  {
+    path: 'options',
+    loadChildren: () =>
+      import('./routed/options/options.module').then((m) => m.OptionsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
