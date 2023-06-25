577bac1c2dc4ce6e912c38bdd42006118d23a101
Eito Takase <eito01178@gmail.com>
2023-06-25 15:52:07 +0900

    selectorを使う

--- src/app/modules/island/island.component.ts
+++ src/app/modules/island/island.component.ts
@@ -4,6 +4,12 @@ import { ThreeService } from '../three/three.service';
 import { IslandService } from './island.service';
 import { islandBaseKey } from 'src/app/constants/model-name';
 import { IslandState } from './island';
+import {
+  selectBeachColor,
+  selectInlandColor,
+  selectSeaColor,
+  selectSkyColor,
+} from './island.selector';
 
 @Component({
   selector: 'app-island',
@@ -28,28 +34,20 @@ export class IslandComponent {
       name: islandBaseKey,
     });
 
-    this.store
-      .select((state) => state.island.inlandColor)
-      .subscribe((inlandColor) => {
-        this.islandService.changeInlandColor(inlandColor);
-      });
+    this.store.select(selectInlandColor).subscribe((inlandColor) => {
+      this.islandService.changeInlandColor(inlandColor);
+    });
 
-    this.store
-      .select((state) => state.island.beachColor)
-      .subscribe((color) => {
-        this.islandService.changeBeachColor(color);
-      });
+    this.store.select(selectBeachColor).subscribe((color) => {
+      this.islandService.changeBeachColor(color);
+    });
 
-    this.store
-      .select((state) => state.island.seaColor)
-      .subscribe((color) => {
-        this.islandService.changeSeaColor(color);
-      });
+    this.store.select(selectSeaColor).subscribe((color) => {
+      this.islandService.changeSeaColor(color);
+    });
 
-    this.store
-      .select((state) => state.island.skyColor)
-      .subscribe((color) => {
-        this.islandService.changeSkyColor(color);
-      });
+    this.store.select(selectSkyColor).subscribe((color) => {
+      this.islandService.changeSkyColor(color);
+    });
   }
 }
---
+++ src/app/modules/island/island.selector.ts
@@ -0,0 +1,29 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { IslandState } from './island';
+
+export const selectIsland = createFeatureSelector<IslandState>('island');
+
+export const selectInlandColor = createSelector(
+  selectIsland,
+  (state) => state.inlandColor
+);
+
+export const selectBeachColor = createSelector(
+  selectIsland,
+  (state) => state.beachColor
+);
+
+export const selectSeaColor = createSelector(
+  selectIsland,
+  (state) => state.seaColor
+);
+
+export const selectSeaAlpha = createSelector(
+  selectIsland,
+  (state) => state.seaAlpha
+);
+
+export const selectSkyColor = createSelector(
+  selectIsland,
+  (state) => state.skyColor
+);
