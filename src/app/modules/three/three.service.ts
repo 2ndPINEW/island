176023d56d7352e7620961bc0eec642957c427ad
Eito Takase <eito01178@gmail.com>
2023-06-25 19:40:22 +0900

    影をつける

--- src/app/modules/three/three.service.ts
+++ src/app/modules/three/three.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { timer } from 'rxjs';
+import { skyObjectKey } from 'src/app/constants/model-name';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
@@ -35,19 +36,33 @@ export class ThreeService {
     const axesHelper = new THREE.AxesHelper(1000);
     this.scene.add(axesHelper);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
     this._objects.push(ambientLight);
     this.scene.add(ambientLight);
 
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(50, 50, 50);
+    const light = new THREE.DirectionalLight(0xffffff, 1.8);
+    light.position.set(-5, 30, 24);
+    light.castShadow = true;
+    light.shadow.mapSize.width = 4096;
+    light.shadow.mapSize.height = 4096;
+    light.shadow.radius = 2;
+
+    light.shadow.camera.left = -80;
+    light.shadow.camera.right = 80;
+    light.shadow.camera.top = 80;
+    light.shadow.camera.bottom = -80;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 80;
+
     this._objects.push(light);
     this.scene.add(light);
 
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
       canvas: canvasElement,
+      alpha: true,
     });
+    (this.renderer as any).shadowMap.enabled = true;
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     timer(0, 16).subscribe(() => {
       this.rerender();
@@ -59,12 +74,24 @@ export class ThreeService {
     loader.load(path, (gltf) => {
       const model = gltf.scene;
       model.name = name;
+      this.enableReceiveShadow(model);
       if (!this.scene) {
         return;
       }
       this._objects.push(model);
       this.scene.add(model);
       this.rerender();
+    });
+  }
+
+  enableReceiveShadow(object: THREE.Object3D) {
+    if (object.name === skyObjectKey) {
+      return;
+    }
+    object.castShadow = true;
+    object.receiveShadow = true;
+    object.children.forEach((child) => {
+      this.enableReceiveShadow(child);
     });
   }
 
